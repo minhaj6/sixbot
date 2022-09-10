e4797160a1fbc760c2f6fa994db475fdcfae386e
Minhaj6 <minhaj.sixbyte@gmail.com>
2022-09-11 02:43:12 +0600

    robot description update

---
+++ build/sixbot/CMakeFiles/sixbot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sixbot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sixbot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
